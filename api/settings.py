2240781cc0b2c98bbb9d10f0fcd8270c5e22f6e7
Angel-Davila <a6519150005@utch.edu.mx>
2021-08-04 11:43:56 -0600

    something ?

--- api/settings.py
+++ api/settings.py
@@ -15,11 +15,10 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
-
 # SECURITY WARNING: keep the secret key used in production secret!
+
 SECRET_KEY = 'django-insecure-5atva19$m+04eiyv6by^*5z%*xm(e018mc!2hd((n9#71+gvj%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
