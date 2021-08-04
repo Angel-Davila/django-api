358f3800e1beb75d0fb4bfdc9d481873ad3b4878
Angel-Davila <a6519150005@utch.edu.mx>
2021-08-04 03:08:25 -0600

    posts database

--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,15 @@
+""" Post Admin Config """
+
 from django.contrib import admin
 
-# Register your models here.
+#Models
+from posts.models import Post
+
+@admin.register(Post)
+class PostsAdmin(admin.ModelAdmin):
+
+    list_display = ['title', 'image', 'likes']
+    list_display_links = ['title']
+    list_editable = ['image', 'likes']
+    search_fields = ['created_at']
+    list_filter = ['created_at']
